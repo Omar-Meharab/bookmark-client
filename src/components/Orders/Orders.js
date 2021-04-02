5dc7b3fa2ecf7271a6c42af46baf4bde316c1b0d
Omar Meharab <omarmeharab81@gmail.com>
2021-04-02 17:53:42 +0600

    a

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,6 +4,28 @@ const Orders = () => {
     return (
         <div>
             <h1>this is orders</h1>
+            <div className="table-div">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Buyer</th>
+                        <th scope="col">Book Name</th>
+                        <th scope="col">Author Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>Otto</td>
+                        <td>Otto</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
         </div>
     );
 };
